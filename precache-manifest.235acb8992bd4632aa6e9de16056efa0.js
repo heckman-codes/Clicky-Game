90c0fb4c740290e049fa78bc21a94347e2cf58c4
heckman-codes <liamheckmancode@gmail.com>
2020-02-16 21:25:37 -0800

    Updates

---
+++ precache-manifest.235acb8992bd4632aa6e9de16056efa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba31adec12b92e875b8fe5f456c92ca2",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "3e978f302780fab9cf3b",
+    "url": "/Clicky-Game/static/css/main.a2a9d95c.chunk.css"
+  },
+  {
+    "revision": "85c17ce4790e9c35c3be",
+    "url": "/Clicky-Game/static/js/2.1861e6df.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Clicky-Game/static/js/2.1861e6df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e978f302780fab9cf3b",
+    "url": "/Clicky-Game/static/js/main.374b2045.chunk.js"
+  },
+  {
+    "revision": "ed33e96b23e5ad0b910f",
+    "url": "/Clicky-Game/static/js/runtime-main.da6617cf.js"
+  }
+]);
